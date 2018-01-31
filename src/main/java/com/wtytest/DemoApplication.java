abe72bd720e8ec81a42c68ba56d8eabdf07cec7f
WangTianyi <tywang@thoughtworks.com>
2018-01-31 22:57:40 +0800

    test aop

--- src/main/java/com/wtytest/DemoApplication.java
+++ src/main/java/com/wtytest/DemoApplication.java
@@ -3,15 +3,15 @@ package com.wtytest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
-@ComponentScan
 @EntityScan
 @EnableJpaRepositories
+@EnableAspectJAutoProxy
 public class DemoApplication {
 
 	@RequestMapping("/api/token")
---
+++ src/main/java/com/wtytest/aop/LoggingAspect.java
@@ -0,0 +1,22 @@
+package com.wtytest.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+
+        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+}
